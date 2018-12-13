9be8afd0b9418b650fd3a303f44c17a55314144f
wildjames <jwild2@sheffield.ac.uk>
2018-12-13 16:53:43 +0000

    Made slicing of eclipses more robust. Rearranged formatting. Removed 'master' stuff.

--- combineData.py
+++ combineData.py
@@ -54,30 +54,19 @@ def tcorrect(tseries, star, observatory, type='B'):
     ts.t = corr.mjd
     return ts
 
+def calc_E(T, T0, P):
+    E = (T-T0) / P
+    return E
+def calc_E_Err(T, T0, P, T_err, T0_err, P_err):
+    N = E(T, T0, P) 
+    err = np.sqrt(
+        ( np.sqrt(T_err**2 + T0_err**2)/(T-T0) )**2 + 
+        (P_err/P)**2
+    )
+    E_err = N * err
+    return E_err
 
-# ------------------- inputted variables-------------------------------------- #
-
-# ## How do we name the files we write out?
-# oname = "MASTER-J2205"
-
-# # Binning
-# binsize = 3
-
-# # Star coordinates, observing location [RA, Dec], extinction coefficient
-# coords = ['22 05 59.48', '-34 14 33.9']
-# obsname = 'lasilla'
-# ext = 0.161 ## Mean value over 20 yrs
-
-# # Ephemeris data
-# T0 = 58404.1329466701
-# period = 0.061286974
-
-# ## List of magnitude corrections ##
-# ref_kappa = [np.nan, -25.9314, -26.1793, -24.1461]
-# # (First entry is left blank, as CCDs are indexed from 1)
-
-# -------------------\inputted variables-------------------------------------- #
-
+    
 def combineData(oname, coords, obsname, T0, period, ref_kappa=None, SDSS=True, std_fname=None, comp_fnames=None, binsize=10, myLoc='.', ext=0.161, fnames=None, std_coords=None, std_mags=None):
     '''
     oname      - Filename template for writing lightcurve plot and data. Appended with binning factor.
@@ -125,8 +114,6 @@ def combineData(oname, coords, obsname, T0, period, ref_kappa=None, SDSS=True, s
         for f in fnames:
             print(f)
         exit()
-
-    print(comp_fnames)
     
     # Writing out
     try:
@@ -150,11 +137,6 @@ def combineData(oname, coords, obsname, T0, period, ref_kappa=None, SDSS=True, s
     print("  Correcting observations from MJD to BMJD (observed from '{}')".format(obsname))
 
     print("  Phase folding data for a T0: {:}, period: {:}".format(T0, period))
-
-    # Ephemeris info
-    eclFile = 'eclipse_times.txt'
-    eclFile = '/'.join([myLoc, eclFile])
-    source_key, eclipse_data = read_ecl_file(eclFile)
     
     # Where are we?
     observatory = coord.EarthLocation.of_site(obsname)
@@ -162,10 +144,6 @@ def combineData(oname, coords, obsname, T0, period, ref_kappa=None, SDSS=True, s
         coords,
         unit=(u.hourangle, u.deg), frame='icrs'
     )
-
-
-    if T0 == None:
-        T0 = 0.0
 
     band = ['', 'r',   'g',     'u'   ]
     c    = ['', 'red', 'green', 'blue']
@@ -183,17 +161,6 @@ def combineData(oname, coords, obsname, T0, period, ref_kappa=None, SDSS=True, s
             CCDs = [str(i) for i in aps]
             CCDs = sorted(CCDs)
 
-            # If we're in the SDSS field, grab the reference stars' magnitudes from their coords.
-            if SDSS:
-                refname         = refname.replace('.log', '.coords')
-                reference_stars = construct_reference(refname)
-            else:
-                reference_stars = get_comparison_magnitudes(std_fname, refname, std_coords=std_coords, 
-                    comp_coords=coords, std_mags=std_mags, obsname=obsname)
-
-            # Plotting area
-            fig, ax = plt.subplots(3, figsize=[12, 8])
-
             # I want altitude converted to zenith angle. Airmass is roughly constant over 
             # a single eclipse so only do it once to save time.
             obs_T = float(data['1'][0][1])
@@ -205,8 +172,19 @@ def combineData(oname, coords, obsname, T0, period, ref_kappa=None, SDSS=True, s
                 fname.split('/')[-1], obs_T.iso, star_loc_AltAz.alt.value, airmass)
             )
 
+            # If we're in the SDSS field, grab the reference stars' magnitudes from their coords.
+            if SDSS:
+                refname         = refname.replace('.log', '.coords')
+                reference_stars = construct_reference(refname)
+            else:
+                reference_stars = get_comparison_magnitudes(std_fname, refname, std_coords=std_coords, 
+                    comp_coords=coords, std_mags=std_mags, obsname=obsname)
+
+            # Plotting area
+            fig, ax = plt.subplots(3, figsize=[12, 8])
+
             # Loop through the CCDs.
-            ### For each CCD, grab the target lightcurve, and the 
+            ### For each CCD, grab the target lightcurve, and the comparisons
             for CCD in CCDs:
                 CCD_int = int(CCD)
 
@@ -247,59 +225,70 @@ def combineData(oname, coords, obsname, T0, period, ref_kappa=None, SDSS=True, s
                 fluxs = sdss_mag2flux(mags)
                 meanFlux = np.mean(fluxs) # Actual FLUX of reference
                 
-
-                print("  CCD {} comparison star magnitudes:".format(CCD))
-                for m, mag in enumerate(mags):
-                    print("    Comparison {}: {:.3f} mag".format(m, mag))
-                print("  ")
-
                 # Conversion of target lightcurve
                 reference = reference / meanFlux # Counts/mJy
                 ratio = target / reference # mJy
 
                 ratio = tcorrect(ratio, star_loc, obsname)
 
-                if False:
-                    # Fold about the period
-                    # ratio = ratio.fold(period, t0=T0) ## BUGGED! and not a LTT error?
-                    fold_time = (((ratio.t - T0) / period) %1)
-                    # fold time domain from -.5 to .5
-                    fold_time[fold_time > 0.5] -= 1
-                    sorted_args = np.argsort(fold_time)
-                    ratio = hcam.hlog.Tseries(
-                        fold_time[sorted_args],
-                        ratio.y[sorted_args],
-                        ratio.ye[sorted_args],
-                        ratio.mask[sorted_args]
-                        )
-                else:
-                    ### Cut out an eclipse, without folding it
-                    # get E from eclipse data, by interpolating. Only do this for the first CCD, since they should all have the same
-                    # timestamps
-                    if CCD == '1':
-                        E = min([t[1] for t in eclipse_data], key=lambda x:abs(x-np.mean(ratio.t)))
-                        E = [t[1] for t in eclipse_data].index(E)
-                        E = eclipse_data[E][0]
-                        
-                        print("  I think that the eclipse spanning from {:.3f} to {:.3f} is cycle number {}".format(
-                            ratio.t[0], ratio.t[-1], E)
-                        )
+                if CCD == '1':
+                    mintime = np.min(ratio.t)
+                    E = calc_E(mintime, T0, period)
+                    E = np.rint(E)
+
+                    while T0 + E*period < ratio.t[0]:
+                        print("  !!! Eclipse time not within these data! Attempting to fix...")
+                        E += 1
+                    while T0 + E*period > ratio.t[-1]:
+                        print("  !!! Eclipse time not within these data! Attempting to fix...")
+                        E -= 1
+                    
+                    print("  I think that the eclipse spanning from {:.3f} to {:.3f} is cycle number {}".format(
+                        ratio.t[0], ratio.t[-1], E)
+                    )
 
                     eclTime = T0 + E*period
+                    print("  The eclipse is then at time {:.3f}".format(eclTime))
 
-                    slice_time = (ratio.t - eclTime) / period
-                    slice_args = (slice_time < 0.5)  *  (slice_time > -0.5)
+                # if False:
+                #     # Fold about the period
+                #     # ratio = ratio.fold(period, t0=T0) ## BUGGED! and not a LTT error?
+                #     fold_time = (((ratio.t - T0) / period) %1)
+                #     # fold time domain from -.5 to .5
+                #     fold_time[fold_time > 0.5] -= 1
+                #     sorted_args = np.argsort(fold_time)
+                #     ratio = hcam.hlog.Tseries(
+                #         fold_time[sorted_args],
+                #         ratio.y[sorted_args],
+                #         ratio.ye[sorted_args],
+                #         ratio.mask[sorted_args]
+                #         )
+                # else:
+                ### Cut out an eclipse, without folding it
+                # get E from eclipse data, by interpolating. Only do this for the first CCD, since they should all have the same
+                # timestamps
+                
+                slice_time = (ratio.t - eclTime) / period
+                slice_args = (slice_time < 0.5)  *  (slice_time > -0.5)
 
-                    ratio = hcam.hlog.Tseries(
-                        ratio.t[slice_args] - eclTime,
-                        ratio.y[slice_args],
-                        ratio.ye[slice_args],
-                        ratio.mask[slice_args]
-                        )
+                ratio = hcam.hlog.Tseries(
+                    slice_time[slice_args],
+                    ratio.y[slice_args],
+                    ratio.ye[slice_args],
+                    ratio.mask[slice_args]
+                    )
+
+                print("  I sliced out {} data from the lightcurve".format(len(ratio.t)))
 
                 # print("  Binning the night of {} by {}".format(fname.split('/')[-1][:-4], binsize))
                 # ratio = ratio.bin(binsize)
                 
+
+                print("  CCD {} comparison star magnitudes:".format(CCD))
+                for m, mag in enumerate(mags):
+                    print("    Comparison {}: {:.3f} mag".format(m, mag))
+                print("  ")
+
                 # Plotting
                 ratio.mplot(ax[CCD_int-1], colour=c[CCD_int])
                 ax[CCD_int-1].set_ylabel('Flux, mJy')
@@ -327,66 +316,70 @@ def combineData(oname, coords, obsname, T0, period, ref_kappa=None, SDSS=True, s
 
             plt.tight_layout()
             pdf.savefig()
+            plt.close()
     print("  ")
-    plt.close(fig)
-
-
-
-    ## <master> is now a dict, containing 3 Tseries objects of the lightcurves.
-
-    # Sort each CCD so all the observations are lined up
-    print("  Creating a master lightcurve, by binning the folded, sum lightcurve")
-    print("   by (number of eclipses +1): {}".format(len(fnames)+1))
-    for CCD in ['1', '2', '3']:
-        lightcurve = master[CCD]
-
-        times = lightcurve.t
-        sorted_args = np.argsort(times)
-        master[CCD] = hcam.hlog.Tseries(
-            times[sorted_args],
-            lightcurve.y[sorted_args],
-            lightcurve.ye[sorted_args],
-            lightcurve.mask[sorted_args]
-        )
-
-        # Bin the lightcurve by the number of nights
-        master[CCD] = master[CCD].bin(len(fnames)+1)
-
-    print("  Done!\n")
-
-    print("  Plotting...")
-    if binsize > 1:
-        oname += '_bin{:02d}'.format(binsize)
-    else:
-        oname += "_unbinned"
-    # Plotting
-    fig, ax = plt.subplots(3, figsize=[24, 8])
-
-    master['1'].mplot(ax[0], colour='red')
-    master['2'].mplot(ax[1], colour='green')
-    master['3'].mplot(ax[2], colour='blue')
-
-    for x in ax:
-        x.set_ylabel('Flux, mJy')
-    ax[2].set_xlabel('Phase, days')
-
-    plt.tight_layout()
-    plt.savefig(oname+'.pdf')
-    # plt.show()
     plt.close('all')
-    print('  Saved to {}'.format(oname+'.pdf'))
-    print("  Done!\n")
-
-    print("  Writing to files...")
-
-    for i, col in zip(['1', '2', '3'], c[1:]):
-        filename = oname+'_{}.calib'.format(col)
-        with open(filename, 'w') as f:
-            f.write("# Phase, Flux, Err_Flux, Mask\n")
-            for t, y, ye, mask in zip(master[i].t, master[i].y, master[i].ye, master[i].mask):
-                f.write("{}, {}, {}, {}\n".format(t, y, ye, mask))
-        print("  Wrote out {}!".format(filename))
-
-    print("\n  Done!")
 
     return written_files
+
+    # ## <master> is now a dict, containing 3 Tseries objects of the lightcurves.
+
+    # # Sort each CCD so all the observations are lined up
+    # print("  Creating a master lightcurve, by binning the folded, sum lightcurve")
+    # masterBin = len(fnames)+1
+    # print("   by (number of eclipses +1): {}".format(masterBin))
+    # for CCD in ['1', '2', '3']:
+    #     print("  MASTER[{}].t has a length {}".format(CCD, len(master[CCD].t)))
+    #     lightcurve = master[CCD]
+
+    #     times = lightcurve.t.copy()
+    #     sorted_args = np.argsort(times)
+    #     print(sorted_args)
+    #     master[CCD] = hcam.hlog.Tseries(
+    #         lightcurve.t[sorted_args],
+    #         lightcurve.y[sorted_args],
+    #         lightcurve.ye[sorted_args],
+    #         lightcurve.mask[sorted_args]
+    #     )
+
+    #     # Bin the lightcurve by the number of nights
+    #     master[CCD] = master[CCD].bin(masterBin)
+
+    # print("  Done!\n")
+
+    # print("  Plotting...")
+    # if binsize > 1:
+    #     oname += '_bin{:02d}'.format(binsize)
+    # else:
+    #     oname += "_unbinned"
+    # # Plotting
+    # fig, ax = plt.subplots(3, figsize=[24, 8])
+
+    # master['1'].mplot(ax[0], colour='red')
+    # master['2'].mplot(ax[1], colour='green')
+    # master['3'].mplot(ax[2], colour='blue')
+
+    # for x in ax:
+    #     x.set_ylabel('Flux, mJy')
+    # ax[2].set_xlabel('Phase, days')
+
+    # plt.tight_layout()
+    # plt.savefig(oname+'.pdf')
+    # # plt.show()
+    # plt.close('all')
+    # print('  Saved to {}'.format(oname+'.pdf'))
+    # print("  Done!\n")
+
+    # print("  Writing to files...")
+
+    # for i, col in zip(['1', '2', '3'], c[1:]):
+    #     filename = oname+'_{}.calib'.format(col)
+    #     with open(filename, 'w') as f:
+    #         f.write("# Phase, Flux, Err_Flux, Mask\n")
+    #         for t, y, ye, mask in zip(master[i].t, master[i].y, master[i].ye, master[i].mask):
+    #             f.write("{}, {}, {}, {}\n".format(t, y, ye, mask))
+    #     print("  Wrote out {}!".format(filename))
+
+    # print("\n  Done!")
+
+    # return written_files
