6c0d4d357ffb058eacdbc46e573e7530f923ac86
wildjames <jwild2@sheffield.ac.uk>
2019-04-04 10:54:49 +0100

    Better reporting, and removing of junk data. Added a LOBF to comparison data

--- combineData.py
+++ combineData.py
@@ -10,10 +10,15 @@ from astropy import time, coordinates as coord, units as u
 from astropy.coordinates import AltAz
 from astropy.stats import sigma_clipped_stats
 
+from scipy.optimize import curve_fit
+
 import hipercam as hcam
 from constructReference import construct_reference, get_comparison_magnitudes
 from getEclipseTimes import read_ecl_file, tcorrect
 from logger import printer
+
+def straight_line(x, A, B): # this is your 'straight line' y=f(x)
+    return A*x + B
 
 def sdss_mag2flux(mag):
     '''Takes an SDSS magnitude, returns the corresponding flux in [mJy]'''
@@ -194,7 +199,6 @@ def combineData(oname, coords, obsname, T0, period, inst='ucam', SDSS=True, std_
     for n, b, col in zip(range(nCCD), band, c):
         printer("  -> CCD {}: {} band, plotted in {}".format(n+1, b, col))
 
-    master = {}
     written_files = []
 
 
@@ -202,6 +206,9 @@ def combineData(oname, coords, obsname, T0, period, inst='ucam', SDSS=True, std_
     print("Making plotting area...", end='')
     plt.ion()
     fig, ax = plt.subplots(nCCD, figsize=[11.69,8.27], sharex=True)
+    # If we only have one CCD, axes still need to be a lists
+    if nCCD == 1:
+        ax = [ax]
 
     twinAx = []
     for i, a in enumerate(ax):
@@ -220,7 +227,6 @@ def combineData(oname, coords, obsname, T0, period, inst='ucam', SDSS=True, std_
     # If we only have one CCD, axes still need to be a lists
     if nCCD == 1:
         compAx = [compAx]
-        ax = [ax]
 
     compFig.tight_layout()
     plt.show()
@@ -298,8 +304,12 @@ def combineData(oname, coords, obsname, T0, period, inst='ucam', SDSS=True, std_
                     comparison = comparison + data.tseries(CCD, a)
                     printer("  The reference star now includes data from aperture {}".format(a))
 
+                sumComparison = copy.deepcopy(comparison)
+
                 # Take the mean
-                comparison = comparison / N
+                if N > 1:
+                    comparison.y  = comparison.y  / float(N)
+                    comparison.ye = comparison.ye / float(N)
 
                 ### <comparison> is now a mean COUNT of the comparison stars for each exposure ###
                 ## Calculate their actual mean flux from their apparent magnitudes
@@ -311,15 +321,15 @@ def combineData(oname, coords, obsname, T0, period, inst='ucam', SDSS=True, std_
 
                 if len(mags) != N:
                     printer("  Target reduction filename:    {}".format(fname))
-                    printer("  Standard reduction filename:  {}".format(refname))
+                    printer("  Comparison stars filename:    {}".format(refname))
                     printer("!!!!! Number of comparison magnitudes in standard star reduction: {}".format(len(mags)))
                     printer("!!!!! Number of comparison stars in target reduction: {}".format(len(ap[1:])))
                     input("Hit <Enter> to continue")
 
                 fluxs = sdss_mag2flux(mags)
                 # Don't take the clipped mean here, as the meanFlux is the mean, mean flux of our comparison stars,
-                #
-                meanFlux  = np.mean(fluxs) # Actual FLUX of constructed comparison star
+                meanFlux  = np.mean(fluxs, dtype=np.float) # Actual FLUX of constructed comparison star
+                sumFlux   = np.sum(fluxs, dtype=np.float)
 
                 ## Reporting
                 printer("  Comparison star apparent SDSS magnitudes:".format())
@@ -337,23 +347,27 @@ def combineData(oname, coords, obsname, T0, period, inst='ucam', SDSS=True, std_
 
 
                 # Conversion of target lightcurve
-                cnt_per_flx = comparison / meanFlux # Counts/mJy
-                ratio = target / cnt_per_flx # mJy
+                cnt_per_flx = sumComparison / sumFlux # Counts/mJy --- float / Tseries is not supported.
+                ratio = target / cnt_per_flx # counts / (counts/mJy) = mJy
 
                 printer("  Correcting data to BMJD time...")
                 ratio = tcorrect(ratio, star_loc, obsname)
 
                 if CCD == '1':
-                    mintime = np.min(ratio.t)
+                    mintime = np.mean(ratio.t)
                     E = calc_E(mintime, T0, period)
                     E = np.rint(E)
+                    printer("  The mean time of this eclipse is {:.3f}.".format(mintime))
+                    printer("  From ephemeris data, I get an eclipse Number,")
+                    printer("    E = ({:.3f} - {:.3f}) / {:.5f}".format(mintime, T0, period))
+                    printer("    E = {}".format(E))
 
                     # The above can be off, if the eclipse isnt the minimum. in/decriment until it's within bounds
                     while T0 + E*period < ratio.t[0]:
-                        printer("  !!! Eclipse time not within these data! Attempting to fix...")
+                        printer("    !!! Eclipse time not within these data! Incrimenting E...")
                         E += 1
                     while T0 + E*period > ratio.t[-1]:
-                        printer("  !!! Eclipse time not within these data! Attempting to fix...")
+                        printer("    !!! Eclipse time not within these data! Decrimenting E...")
                         E -= 1
 
                     printer("  I think that the eclipse spanning from {:.3f} to {:.3f} is cycle number {}".format(
@@ -365,7 +379,8 @@ def combineData(oname, coords, obsname, T0, period, inst='ucam', SDSS=True, std_
 
                     printer("")
 
-
+                # slice out the data between phase -0.5 and 0.5
+                printer("  Slicing out data between phase -0.5 -> +0.5")
                 slice_time = (ratio.t - eclTime) / period
                 slice_args = (slice_time < 0.5)  *  (slice_time > -0.5)
 
@@ -376,49 +391,81 @@ def combineData(oname, coords, obsname, T0, period, inst='ucam', SDSS=True, std_
                     ratio.mask[slice_args]
                     )
 
+                printer("  Removing null data")
+                # If data is bad, i.e. 0, mask it
+                slice_args = np.where((ratio.y != 0) * (ratio.ye != 1) * (ratio.y > 0.0))
+                ratio = hcam.hlog.Tseries(
+                    ratio.t[slice_args],
+                    ratio.y[slice_args],
+                    ratio.ye[slice_args],
+                    ratio.mask[slice_args]
+                    )
+
+                ratio.mask[:] = 0
+
                 printer("  I sliced out {} data from the lightcurve about the eclipse.".format(len(ratio.t)))
 
                 # Plotting management
                 ax[CCD_int].clear()
                 if CCD_int == 0:
                     ax[0].set_title(fname.split('/')[-1])
-                    compAx[0].set_title("{}\nCCD {}, comparison star".format(fname.split('/')[-1], CCD))
+                    compAx[0].set_title("{}\nCCD {}, comparison stars, normalised.".format(fname.split('/')[-1], CCD))
                 ax[CCD_int].set_ylabel('Flux, mJy')
 
-                # Scale the right labels
+                # Scale the right side labels
                 twinAx[CCD_int].set_ylim( ax[CCD_int].get_ylim() / meanFlux )
 
                 # Plot the ratio
                 ratio.mplot(ax[CCD_int], colour=c[CCD_int])
 
-                # # Plot the mean count flux on the figure -- only used when single aperture, as not as useful as ratios
+                compMin =  9e99
+                compMax = -9e99
                 if len(ap) == 2:
-                    compAx[CCD_int].scatter(comparison.t, comparison.y, s=5, label='Mean', color='black')
+                    # # Plot the mean count flux on the figure -- only used when single aperture, as not as useful as ratios
+                    compAx[CCD_int].errorbar(comparison.t, comparison.y, yerr=comparison.ye,
+                        label='Mean', color='black', linestyle='', marker='o', capsize=0)
                     compAx[CCD_int].axhline(np.mean(comparison.y), linestyle='--', color='black')
+                    compMin = np.min(comparison.y)
+                    compMax = np.max(comparison.y)
                 else:
                     # Plot each combination of comparison star ratios, i.e. for 3 comparisons: 2/3, 2/4, 3/4
+                    j = 0
                     for i, a in enumerate(ap[1:-1]):
                         first = data.tseries(CCD, a)
                         for b in ap[i+2:]:
-                            print("Plotting ap {}/{}".format(a, b))
+                            print("  -> Plotting ap {}/{}".format(a, b))
                             toPlot = first / data.tseries(CCD, b)
+
+                            toPlot.y = toPlot.y / np.mean(toPlot.y)
+                            toPlot.y = toPlot.y + (float(j) / 5)
+                            j += 1
+
+                            # Get min and max axis limits
+                            if np.max(toPlot.y) > compMax:
+                                compMax = np.max(toPlot.y)
+                            if np.min(toPlot.y) < compMin:
+                                compMin = np.min(toPlot.y)
+
+                            # Fit a straight line to the data. Deviations indicate bad comparisons
+                            A,B = curve_fit(straight_line, toPlot.t, toPlot.y)[0]
+                            fit_X = np.linspace(toPlot.t[0], toPlot.t[-1], 3)
+                            fit_Y = straight_line(fit_X, A, B)
 
                             mean, _, _ = sigma_clipped_stats(toPlot.y, maxiters=2, sigma=3)
                             compAx[CCD_int].axhline(mean, linestyle='--', color='black')
+                            compAx[CCD_int].plot(fit_X, fit_Y, color='red', linestyle=':')
                             compAx[CCD_int].scatter(toPlot.t, toPlot.y,
                                 s=10,
-                                label="Aperture {}/{}".format(a, b),
-                                alpha=0.6,
-                                color='red'
+                                label="Aperture {}/{} - grad: {:.2f}".format(a, b, A),
+                                alpha=0.6
                             )
 
                 # Add in legend artist
                 compAx[CCD_int].legend()
 
-                # recompute the ax.dataLim
-                compAx[CCD_int].relim()
-                # update ax.viewLim using the new dataLim
-                compAx[CCD_int].autoscale_view()
+                pad = 0.05 * (compMax - compMin)
+                compAx[CCD_int].set_ylim([compMin-pad, compMax+pad])
+                compAx[CCD_int].set_xlim([comparison.t[0], comparison.t[-1]])
 
                 # File handling stuff
                 b = band[CCD_int]
@@ -431,6 +478,7 @@ def combineData(oname, coords, obsname, T0, period, inst='ucam', SDSS=True, std_
                 filename = "{}_{}{}.calib".format(filename, fname.split('/')[-1][:-4], b)
 
                 # Saving data
+                printer("  These data have {} masked points.".format(np.sum(ratio.mask > 0)))
                 with open(filename, 'w') as f:
                     f.write("# Phase, Flux, Err_Flux\n")
                     for t, y, ye, mask in zip(ratio.t, ratio.y, ratio.ye, ratio.mask):
@@ -438,18 +486,7 @@ def combineData(oname, coords, obsname, T0, period, inst='ucam', SDSS=True, std_
                             f.write("{} {} {}\n".format(t, y, ye))
 
                 written_files.append(filename)
-
-                # Store the lightcurve in the master dict
-                try:
-                    # If we find <master> has an entry for this CCD, append it
-                    master[CCD] = master[CCD].append(ratio)
-                    printer("  Appended CCD {} to the data dict.".format(CCD))
-                except KeyError:
-                    # Otherwise, create a new entry
-                    printer("  Adding {} to the data dict...".format(CCD))
-                    master[CCD] = ratio
-
-
+                printer("  Wrote data to {}".format(filename))
                 printer("  Finished CCD {}\n".format(CCD))
 
             ax[-1].set_xlabel('Phase, days')
