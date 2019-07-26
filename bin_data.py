48a97ab5a36ff2608b23cc2fbd1bd5f191d09296
wildjames <jwild2@sheffield.ac.uk>
2019-07-26 12:02:09 +0100

    Altered to take command line args, rather than needing editing each time

--- bin_data.py
+++ bin_data.py
@@ -2,122 +2,168 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-files = [
-    # 'SDSSJ0748_ucam_2017-03-20_g.calib',
-    'SDSSJ0748_ucam_2017-03-20_r.calib',
-    # 'SDSSJ0748_ucam_2017-03-20_u.calib',
-]
-nbins = 350
-phi_min = -0.2
-phi_max = +0.3
-
-print("Binning into {} bins between phase {} and {}".format(nbins, phi_min, phi_max))
-
-# plotting area. Top axis will have output, bottom will have input.
-fig, ax = plt.subplots(2, figsize=[8, 6], sharex=True, sharey=True)
-ax[0].set_title('Binning down to {} points'.format(nbins))
+import argparse
 
 
-# Slap all the data in a dict
-master_data = {
-    'ts': [],
-    'fl': [],
-    'fe': []
-}
+if __name__ == "__main__":
 
-lab = True
-for f in files:
-    data = np.loadtxt(f, delimiter=' ')
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        'nbin',
+        help='The number of bins the data will be divided into',
+        type=int,
+        default=350
+    )
+    parser.add_argument(
+        'phi',
+        help='The phase range of the files will be truncated to within MIN MAX',
+        type=float,
+        nargs=2,
+        default=[-np.inf, np.inf]
+    )
+    parser.add_argument(
+        'files',
+        help='The files to be binned together',
+        type=str,
+        nargs='+'
+    )
 
-    master_data['ts'].extend(data[:,0])
-    master_data['fl'].extend(data[:,1])
-    master_data['fe'].extend(data[:,2])
+    args = parser.parse_args()
 
-    ax[1].step(data[:,0], data[:,1], label=f)
-    if lab:
-        ax[0].step(data[:,0], data[:,1], color='lightgrey', label='Input Data')
-        lab = False
-    else:
-        ax[0].step(data[:,0], data[:,1], color='lightgrey')
+    nbins = args.nbin
+    phi_min, phi_max = args.phi
+    files = args.files
 
-print("Input data have a {} points".format(len(master_data['ts'])))
 
-# Lets work with arrays, shall we?
-master_data = pd.DataFrame(master_data)
+    print("Binning into {} bins between phase {} and {}".format(nbins, phi_min, phi_max))
 
-# Sort the master DF
-master_data.sort_values('ts', ascending=True, inplace=True)
+    # plotting area. Top axis will have output, bottom will have input.
+    fig, ax = plt.subplots(2, figsize=[8, 6], sharex=True, sharey=True)
+    ax[0].set_title('Binning down to {} points'.format(nbins))
 
-# I'll bin onto this axis...
-out_X = np.linspace(phi_min, phi_max, nbins)
-# I need to add an extra value to the top and tail of the linspace, or digitize gives me the values out to +/- inf
-sep = np.mean(out_X[1:] - out_X[:-1])
-out_X = np.insert(out_X, 0, out_X[0]-sep)
-out_X = np.append(out_X, out_X[-1]+sep)
 
-# Which bins do the values go into? The above defines bin edges!
-inds = np.digitize(master_data['ts'], out_X)
+    # Slap all the data in a dict
+    master_data = {
+        'ts': [],
+        'fl': [],
+        'fe': []
+    }
 
-# Initial dict
-binned_master = pd.DataFrame(data=None,
-    columns=master_data.columns)
+    lab = True
+    for f in files:
+        print("-", f)
+        data = np.loadtxt(f, delimiter=' ')
 
-# Need to go from 1 (the 0th index is junk, extending off to -inf), to the value of nbins (avoid OBOE)
-for ibin in range(1, nbins+1):
-    # Which data?
-    indeces = np.where(inds==ibin)
-    # Grab data
-    sliced = master_data.iloc[indeces]
-    # Mean data
-    mean = sliced.mean()
+        master_data['ts'].extend(data[:,0])
+        master_data['fl'].extend(data[:,1])
+        master_data['fe'].extend(data[:,2])
 
-    # The mean above does not handle errors correctly. Let's do that;
-    # X = A+B; dX^2 = xA^2 + dB^2
-    err = np.array(sliced['fe']) **2
-    err = np.sum(err)
-    err = np.sqrt(err)
-    mean['fe'] = err
+        ax[1].step(data[:,0], data[:,1], label=f)
+        if lab:
+            ax[0].step(data[:,0], data[:,1], color='lightgrey',
+            where='mid', label='Input Data')
+            lab = False
+        else:
+            ax[0].step(data[:,0], data[:,1], where='mid', color='lightgrey')
 
-    # Add to binned df. Ignore index must be enabled for some reason.
-    binned_master = binned_master.append(mean, ignore_index=True)
+    print("Input data have a {} points".format(len(master_data['ts'])))
 
-# Step plots are physically reasonable
-ax[0].step(binned_master['ts'], binned_master['fl'], color='black', where='mid', label='Binned lightcurve')
+    # Lets work with arrays, shall we?
+    master_data = pd.DataFrame(master_data)
 
-# Don't print errorbars if they're too tiny?
-all_mean = binned_master.mean()
-if all_mean['fe'] > all_mean['fl']/10.:
-    print("Signal/error ratio: {} -- Plotting error bars".format(all_mean['ff']/all_mean['fe']))
-    ax[0].errorbar(binned_master['ts'], binned_master['fl'],
-        yerr=binned_master['fe'], fmt='none', ecolor='black', capsize=0)
+    # Sort the master DF
+    master_data.sort_values('ts', ascending=True, inplace=True)
 
-# Stretch the x-axis a little, to make it easier to see what's what
-extension = np.max([abs(0.1*phi_min), abs(0.1*phi_max)])
-lims = [phi_min - extension, phi_max + extension]
-ax[0].set_xlim(lims)
+    # Enforce the phase limits
+    master_data = master_data.loc[(master_data['ts'] >= phi_min) & (master_data['ts'] <= phi_max)]
 
-ax[0].legend()
-ax[1].legend()
-plt.tight_layout()
-# block=false so that the user can see the plot while deciding stuff
-plt.show(block=False)
+    # I'll bin onto this axis...
+    out_X = np.linspace(phi_min, phi_max, nbins)
+    # I need to add an extra value to the top and tail of the linspace, or digitize gives me the values out to +/- inf
+    sep = np.mean(out_X[1:] - out_X[:-1])
+    out_X = np.insert(out_X, 0, out_X[0]-sep)
+    out_X = np.append(out_X, out_X[-1]+sep)
 
-cont = input("Write to a file? y/n: ")
-if cont.lower()[0] == 'y':
-    oname = input("Enter a filename: ")
-    oname += '.calib'
-    with open(oname, 'w') as f:
-        f.write("# This file was produced by binning the following files down to {} points:\n".format(nbins))
-        for cf in files:
-            f.write("# {}\n".format(cf))
-        f.write("#\n# phase, flux, error\n")
-        for _, row in binned_master.iterrows():
-            t = row['ts']
-            fl = row['fl']
-            fe = row['fe']
-            f.write("{} {} {}\n".format(t, fl, fe))
+    # Which bins do the values go into? The above defines bin edges!
+    inds = np.digitize(master_data['ts'], out_X)
+    master_data['bin'] = inds
 
-    figname = "../figs/"+oname.replace('.calib', '.pdf')
-    plt.savefig(figname)
+    # These functions will be applied to the dataframe when taking means.
+    def weighted_mean(x):
+        return np.average(
+            x,
+            weights=master_data.loc[x.index, 'fe']**-2.
+        )
+    def calc_errs(x):
+        d = master_data.loc[x.index, 'fe']
+        return np.sqrt(np.sum(d**2)) / len(d)
 
-plt.close()
+    # This dict tells the agg what function to use when aggregating each column
+    func_dict = {'fl': weighted_mean,
+                'ts': weighted_mean,
+                'fe': calc_errs}
+
+    # Take a weighted mean of each bin.
+    binned_master = master_data.groupby('bin').agg(weighted_mean)
+    binned_master.sort_values('ts', ascending=True, inplace=True)
+
+    # Enforce the phase limits.
+
+
+    # Step plots are physically reasonable
+    ax[0].step(
+        binned_master['ts'], binned_master['fl'],
+        color='black', where='mid', label='Binned lightcurve',
+        zorder=10
+    )
+    ax[0].errorbar(
+        binned_master['ts'], binned_master['fl'],
+        binned_master['fe'],
+        color='black', fmt='none', alpha=0.7,
+        label=None,
+        zorder=10
+    )
+
+    # Don't print errorbars if they're too tiny?
+    all_mean = binned_master.mean()
+    if all_mean['fe'] > all_mean['fl']/10.:
+        print("Signal/error ratio: {} -- Plotting error bars".format(all_mean['fl']/all_mean['fe']))
+        ax[0].errorbar(
+            binned_master['ts'], binned_master['fl'],
+            yerr=binned_master['fe'], fmt='none',
+            ecolor='black', capsize=0,
+            zorder=9, label=None
+        )
+
+    # Stretch the x-axis a little, to make it easier to see what's what
+    extension = np.max([abs(0.1*phi_min), abs(0.1*phi_max)])
+    lims = [phi_min - extension, phi_max + extension]
+    ax[0].set_xlim(lims)
+
+    ax[0].legend()
+    ax[1].legend()
+    plt.tight_layout()
+    # block=false so that the user can see the plot while deciding stuff
+    plt.show(block=False)
+
+    cont = input("Write to a file? y/n: ")
+    if cont.lower().startswith('y'):
+        oname = input("Enter a filename: ")
+        if oname == '':
+            exit()
+        oname += '.calib'
+        with open(oname, 'w') as f:
+            f.write("# This file was produced by binning the following files down to {} points between phase {} and {}:\n".format(nbins, phi_min, phi_max))
+            for cf in files:
+                f.write("# {}\n".format(cf))
+            f.write("#\n# phase, flux, error\n")
+            for _, row in binned_master.iterrows():
+                t = row['ts']
+                fl = row['fl']
+                fe = row['fe']
+                f.write("{} {} {}\n".format(t, fl, fe))
+
+        figname = oname.replace('.calib', '.pdf')
+        plt.savefig(figname)
+
+    plt.close()
