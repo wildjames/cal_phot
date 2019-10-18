e33a77a246691532821f3ae28dcbd021a8e89559
wildjames <jwild2@sheffield.ac.uk>
2019-10-18 11:42:51 +0100

    Fully implimented YAML interpreter

--- cal_phot.py
+++ cal_phot.py
@@ -1,6 +1,7 @@
 from ruamel import yaml
 import argparse
 from pathlib import Path
+import os
 
 from getEclipseTimes import getEclipseTimes
 from extractData import extract_data
@@ -48,6 +49,7 @@ if __name__ in "__main__":
         'oname': "Reduced_system",
         'get_eclipse_times': False,
         'fit_ephemeris': False,
+        'flux_calibrate': True
     }
     for key, value in payload_defaults.items():
         for payload_key, payload in to_extract.items():
@@ -68,6 +70,12 @@ if __name__ in "__main__":
     T0 = input_dict['t0']
     period = input_dict['period']
 
+    # Create the working directory if needed
+    if not os.path.isdir(directory):
+        os.mkdir(directory)
+
+
+    # Do the eclipse times, where needed
     for key, payload in to_extract.items():
         print("Data: {}".format(key))
         pprint(payload)
@@ -93,5 +101,50 @@ if __name__ in "__main__":
 
             getEclipseTimes(fnames, target_coords, obsname, myLoc=directory)
 
+    # Fit ephemeris
     if do_fit_ephem:
         T0, period = fitEphem(directory, T0, period)
+
+    extracted_files = []
+    # Extract the data for each payload
+    for key, payload in to_extract.items():
+        oname = payload['oname']
+        observatory = payload['observatory']
+        instrument = payload['inst']
+
+        fnames = payload['logfiles']
+        no_calibration = not payload['flux_calibrate']
+
+        if no_calibration:
+            written_files = extract_data(
+                oname, target_coords, observatory, T0, period,
+                inst=instrument, SDSS=True, fnames=fnames,
+                myLoc=directory
+            )
+            extracted_files += written_files
+
+        elif is_SDSS:
+            written_files = extract_data(
+                oname, target_coords, observatory, T0, period,
+                inst=instrument, SDSS=is_SDSS, myLoc=directory,
+                fnames=fnames
+            )
+            extracted_files += written_files
+
+        else:
+            comparisons = payload['comparison logfiles']
+            std_logfile = payload['standard logfile']
+            std_coords = payload['standard coords']
+            std_mags = payload['standard mags']
+
+            written_files = extract_data(
+                oname, target_coords, observatory, T0, period,
+                SDSS=is_SDSS, myLoc=directory, fnames=fnames,
+                comp_fnames=comparisons, inst=instrument,
+                std_fname=std_logfile, std_coords=std_coords, std_mags=std_mags
+            )
+
+    if len(written_files):
+        print("I created the following files:")
+        for f in written_files:
+            print(" -> {}".format(f))
